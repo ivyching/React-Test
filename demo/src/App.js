45005b8228ec05585b51a2b0d42eeddb19188884
ivy.huang <ivy.huang@wakeasp.com.tw>
2021-01-08 14:12:27 +0800

    counter-click sample

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [count, setCount] = React.useState(0);
   return (
-    <div className="App">
-      <h1>Learn React Testing!!</h1>
-    </div>
+      <div data-test="component-app" className="App">
+          <h1 data-test="counter-display">
+              The counter is currently&nbsp;
+              <span data-test="count">{count}</span>
+          </h1>
+          <button
+              data-test="increment-button"
+              onClick={() => setCount(count + 1)}
+          >
+              Increment counter
+          </button>
+      </div>
   );
 }
 
