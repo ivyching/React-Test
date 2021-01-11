508ef126e0fca43c5d4b30dcb63ae1c2cc222e8a
ivy.huang <ivy.huang@wakeasp.com.tw>
2021-01-11 17:19:12 +0800

    test button and counter exist

--- demo/src/App.js
+++ demo/src/App.js
@@ -6,7 +6,8 @@ function App() {
     const [count, setCount] = React.useState(0);
   return (
       <div data-test="component-app" className="App">
-
+        <h1 data-test="counter-display">The counter is currently</h1>
+          <button data-test="increment-button">counter+</button>
       </div>
   );
 }
--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -2,6 +2,7 @@
 import App from './App';
 import Enzyme,{shallow} from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from "react";
 
 Enzyme.configure({adapter:new EnzymeAdapter()});
 
@@ -16,11 +17,19 @@ test("renders without errors",()=>{
 //按鈕是否顯示
 test("render button",()=>{
 
+  const wrapper=shallow(<App/>);
+
+  const button=wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
 });
 
 //counter 顯示的數字是否正確
 test("counter display",()=>{
 
+  const wrapper=shallow(<App/>);
+
+  const counterDisplay=wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
 });
 
 //counter 的初始值是否為0
