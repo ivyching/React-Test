379e30e73725df453ca5c1e751c463574023bcfd
ivy.huang <ivy.huang@wakeasp.com.tw>
2021-01-11 16:51:26 +0800

    babel 套件安裝 react remove properties

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -5,11 +5,29 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter:new EnzymeAdapter()});
 
-test('renders learn react link', () => {
+test("renders without errors",()=>{
 
-  const wrapper= shallow(<App/>)
+  const wrapper=shallow(<App/>);
 
-  console.log(wrapper.debug());
-  expect(wrapper).toBeTruthy();
+  const appComponent=wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+//按鈕是否顯示
+test("render button",()=>{
+
+});
+
+//counter 顯示的數字是否正確
+test("counter display",()=>{
+
+});
+
+//counter 的初始值是否為0
+test("counter start at 0",()=>{
+
+});
+//按下按鈕 counter是否加一
+test("clicking on button increments counter display",()=>{
 
 });
