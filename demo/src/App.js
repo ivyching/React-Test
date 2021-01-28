5be4863f174d9013ef37d821b9b43c44e1fd1afe
ivy.huang <ivy.huang@wakeasp.com.tw>
2021-01-28 16:44:14 +0800

    test count and click

--- demo/src/App.js
+++ demo/src/App.js
@@ -6,8 +6,8 @@ function App() {
     const [count, setCount] = React.useState(0);
   return (
       <div data-test="component-app" className="App">
-        <h1 data-test="counter-display">The counter is currently</h1>
-          <button data-test="increment-button">counter+</button>
+        <h1 data-test="counter-display">The counter is currently  <span data-test="count">{count}</span></h1>
+          <button onClick={()=>{setCount(count+1)}} data-test="increment-button">counter+</button>
       </div>
   );
 }
--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -6,37 +6,56 @@ import React from "react";
 
 Enzyme.configure({adapter:new EnzymeAdapter()});
 
+const setup=()=>shallow(<App/>);
+const findByTestAttribute=(wrapper,value)=>wrapper.find(`[data-test='${value}']`);
+
 test("renders without errors",()=>{
 
-  const wrapper=shallow(<App/>);
+  const wrapper=setup();
 
-  const appComponent=wrapper.find("[data-test='component-app']");
+  const appComponent=findByTestAttribute(wrapper,'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 //按鈕是否顯示
 test("render button",()=>{
 
-  const wrapper=shallow(<App/>);
+  const wrapper=setup();
 
-  const button=wrapper.find("[data-test='increment-button']");
+  const button=findByTestAttribute(wrapper,'increment-button');
   expect(button.length).toBe(1);
 });
 
 //counter 顯示的數字是否正確
 test("counter display",()=>{
 
-  const wrapper=shallow(<App/>);
+  const wrapper=setup();
 
-  const counterDisplay=wrapper.find("[data-test='counter-display']");
+  const counterDisplay=findByTestAttribute(wrapper,'counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 
 //counter 的初始值是否為0
 test("counter start at 0",()=>{
 
+  const wrapper=setup();
+
+  const count=findByTestAttribute(wrapper,"count").text();
+
+  expect(count).toBe("0");
+
 });
 //按下按鈕 counter是否加一
 test("clicking on button increments counter display",()=>{
+
+  const wrapper = setup();
+
+  const button=findByTestAttribute(wrapper,'increment-button');
+
+  button.simulate('click');
+
+  const count=findByTestAttribute(wrapper,"count").text();
+
+  expect(count).toBe("1");
 
 });
